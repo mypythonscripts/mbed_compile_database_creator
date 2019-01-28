b1331761a92ff18e0ffce6e63aa1973b447bef36
CO\MM07860 <shuji_mikami@murata.com>
2019-01-28 12:44:53 +0900

    初回

---
+++ createJson.py
@@ -0,0 +1,85 @@
+# coding: UTF-8
+
+import os
+
+#file readout
+path = './Make.log'
+print('Step0 : MakeLogFile read')
+logFile = open(path, mode='r')
+
+lines = logFile.readlines()
+
+jsonPath = 'compile_commands.json'
+jsonFile = open(jsonPath, mode='w')
+jsonFile.write('[')
+
+#compiler
+compilers = ['arm-none-eabi-gcc', 'arm-none-eabi-g++']
+
+#extensions
+extensions = ['.c', '.cpp', '.s']
+
+cnt = 0
+for line in lines:
+    #check if the line starts with compiler name
+    isCompilerMessage = False 
+    for compiler in compilers:
+        if line.find(compiler, 0) == 0:
+            isCompilerMessage = True
+            break
+    
+    if isCompilerMessage == True:
+        #if not first comma
+        if cnt > 0:
+            jsonFile.write(',')
+
+        #start json database
+        jsonFile.write('\n\t{\n')
+
+        #directory
+        jsonFile.write('\t\t"directory": ')
+        currentDirectory = str(os.getcwd()).replace('\\', '/')
+        jsonFile.write('"' + currentDirectory + '",')
+        jsonFile.write('\n')
+
+        #start compile commands
+        jsonFile.write('\t\t"arguments": [\n')
+
+        #targetFilePath
+        targetFilePath = ''
+
+        #split compile commands
+        commandsCount = 0
+        commands = line.split(' ')
+        for option in commands:
+            if commandsCount > 0:
+                jsonFile.write(',\n')
+
+            jsonFile.write('\t\t\t' + '"' + str(option).strip('\r\n') + '"')
+            commandsCount+=1
+
+            #save file fullpath
+            isSourceFileName = False
+            for extension in extensions:
+                if str(option).strip('\r\n').endswith(extension) == True:
+                    targetFilePath = str(option).strip('\r\n')
+                    break
+
+
+
+        #end compile commands
+        jsonFile.write('\n')
+        jsonFile.write('\t\t],\n')
+        
+        #target sourcename
+        jsonFile.write('\t\t"file": ')
+        jsonFile.write('"' + targetFilePath + '"')
+        jsonFile.write('\n')
+
+        #end json database
+        jsonFile.write('\t}')
+
+        cnt+=1
+#end jsondatabase
+jsonFile.write('\n')        
+jsonFile.write(']\n')
