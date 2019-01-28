f0bd6a22d50cc4cea655a98208e80d7d25686472
CO\MM07860 <shuji_mikami@murata.com>
2019-01-28 14:15:34 +0900

    ディレクトリ移動を検知してその結果をjsonに反映するように

--- createJson.py
+++ createJson.py
@@ -2,9 +2,27 @@
 
 import os
 
+ 
+def getChangedDirectory(commandStr):
+    """
+    Return directory moved in makefile command.
+
+    Parameters
+    ----------
+    commandStr : str
+        makefile compile command string
+    """
+
+    if str(commandStr).find('-C') >= 0:
+        commandArray = str(commandStr).split(' ')
+        
+        index = commandArray.index('-C')
+
+        return commandArray[index + 1]
+
+
 #file readout
 path = './Make.log'
-print('Step0 : MakeLogFile read')
 logFile = open(path, mode='r')
 
 lines = logFile.readlines()
@@ -19,8 +37,15 @@ compilers = ['arm-none-eabi-gcc', 'arm-none-eabi-g++']
 #extensions
 extensions = ['.c', '.cpp', '.s']
 
+#currentDirectory
+currentDirectory = str(os.getcwd()).replace('\\', '/')
+
 cnt = 0
 for line in lines:
+    #check the direcory change
+    if getChangedDirectory(line) != None:
+        currentDirectory = getChangedDirectory(line)
+
     #check if the line starts with compiler name
     isCompilerMessage = False 
     for compiler in compilers:
@@ -38,7 +63,6 @@ for line in lines:
 
         #directory
         jsonFile.write('\t\t"directory": ')
-        currentDirectory = str(os.getcwd()).replace('\\', '/')
         jsonFile.write('"' + currentDirectory + '",')
         jsonFile.write('\n')
 
@@ -80,6 +104,6 @@ for line in lines:
         jsonFile.write('\t}')
 
         cnt+=1
-#end jsondatabase
+#end jsonfile
 jsonFile.write('\n')        
 jsonFile.write(']\n')
